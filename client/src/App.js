3af1bf2afd151e9d4baacac7b2fc1e0cbfbadc17
sahmed <sahmed@alumni.depaul.edu>
2019-03-06 20:20:44 -0600

    basic structure, navbar component, images, json done.

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import Navbar from "./components/Navbar";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    topscore:0,
+    score:0
+
+  };
+
+  removeFriend = id => {
+   //shuffle
+   friends.sort(() => Math.random() - 0.5);
+    // Set this.state.friends equal to the new friends array
+    //globle enviornment that saves all the updates
+    this.setState({ friends });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Navbar> </Navbar>
+        <Title>Pokémon-Clicky-Game (Characters List)</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+          removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            
+          />
+        ))}
+        
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function  Navbar() {
+  return (<div className="navbar">
+      <ul>
+       <li ><a href="/"> Pokémon Edition Clicky Game </a></li>
+        <li class>Do NOT click on the same image Twice!</li>
+         <li> Score: </li>
+         <li> Top Score: </li>
+      </ul>
+     </div>
+  );
+}
+
+export default  Navbar;
+
+
+
